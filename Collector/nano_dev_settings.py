37e7c30c09ca0f39f3e1fd1eded0c9e1e30f8d42
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2016-10-15 15:20:30 -0300

    Personal mail settings changed

--- Collector/nano_dev_settings.py
+++ Collector/nano_dev_settings.py
@@ -2,7 +2,7 @@ from .settings import *
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "rognoni.ignacio@gmail.com"
-EMAIL_HOST_PASSWORD = "irognoni1991"
+EMAIL_HOST_USER = "gmail@email.com"
+EMAIL_HOST_PASSWORD = "gmail password"
 EMAIL_PORT = 587
-EMAIL_RECEIVER = "rognoni.ignacio@gmail.com"
+EMAIL_RECEIVER = "mail@receiver"
