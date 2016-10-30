8f36bfd757a77b131129a844e51b5377c2113fdf
Agustin Castro <agustin.castro.91@gmail.com>
2016-10-29 21:26:39 -0300

    Finished logout using django auth functions

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.Login, name='login'),
+    url(r'^logout/$', auth.logout_then_login, name='logout'),
     url(r'^register/$', views.Register.as_view(),  name='register'),
     url(r'^reset_password/$', views.ResetPasswordRequestView.as_view(), name="password_reset"),
     url(r'^reset_password_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', views.PasswordResetConfirmView.as_view(), name='reset_password_confirm'),
