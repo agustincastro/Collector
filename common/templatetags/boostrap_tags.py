5dea186bc41af036e9f3c25cd355e6d2f603456b
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2016-11-06 22:45:10 -0300

    Added bootstrap classes to contact form

--- common/templatetags/boostrap_tags.py
+++ common/templatetags/boostrap_tags.py
@@ -1,12 +1,15 @@
 from django import template
+
 register = template.Library()
+
 
 @register.filter(name='addcss')
 def addcss(field, css):
-   return field.as_widget(attrs={"class":css})
+    return field.as_widget(attrs={"class": css})
+
 
 @register.filter(name='placeholder')
 def placeholder(field, args=None):
     if args == None:
         return field
-    return field.field.widget.attrs.update({ "placeholder": args })
+    return field.field.widget.attrs.update({"placeholder": args})
